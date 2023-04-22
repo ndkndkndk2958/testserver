8b41e09eb174423d4dfc78b0cf415eb592c713f9
ndkndkndk2958 <107983161+ndkndkndk2958@users.noreply.github.com>
2023-04-23 04:15:17 +0700

    Update server.py

--- server.py
+++ server.py
@@ -83,7 +83,6 @@ def add_total_hearts():
     return 'add total hearts!'
 
 if __name__ == '__main__':
-    app = Flask(__name__)
     socketio.run(app, debug=True,port=8000)
     app.debug = True
     http_server = WSGIServer(('', 8000), app, handler_class=WebSocketHandler)
